3fb05227033b60a9e4e27f8e0500bfa64cf18952
Callum Joseph Morgan <callumjoseph97@gmail.com>
2019-06-19 16:12:52 +0100

    Have updated the next.config.js to sort issue with target and withCSS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withCSS = require('@zeit/next-css')
 
-module.exports = {
-    target: 'serverless',
-    withCSS
-}
-module.exports = withCSS;
+module.exports = withCSS({
+    target: 'serverless'  
+})
